b10c2f1039f6492ca22fef2e1c0eab44ffd68225
imrajat-sharma <rajat.s082005@gmail.com>
2026-02-24 10:09:35 +0530

    new commit

--- 12_Session_&_Cookies/app.js
+++ 12_Session_&_Cookies/app.js
@@ -1,84 +1,85 @@
 const express = require("express");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
-const bcryptjs = require("bcryptjs")
-const UserModel = require("./models/UserModel")
+const bcryptjs = require("bcryptjs");
+const UserModel = require("./models/UserModel");
 const app = express();
 require("dotenv").config();
 
-const { isAuth } = require("./middlewares/isAuth")
+const { isAuth } = require("./middlewares/isAuth");
 
-app.use(express.json())
+app.use(express.json());
 
 const store = new MongoDBStore({
   uri: process.env.MONGOURI,
-  collection: "sessions"
+  collection: "sessions",
 });
 
-app.use(session({
-  secret: "thissessionsecret",
-  resave: false,
-  saveUninitialized: true,
-  store: store,
-  cookie: { secure: false } //true in https
-}));
-
+app.use(
+  session({
+    secret: "thissessionsecret",
+    resave: false,
+    saveUninitialized: true,
+    store: store,
+    cookie: { secure: false }, //true in https
+  }),
+);
 
 app.get("/dashboard", isAuth, (req, res) => {
-    if(!req.session.isAuth) {
-        return res.status(401).json({ message: "Unauthorized access!"})
-    }
-    res.json({ message: "Welcome to the session and cookies demo!"})
-})
-
+  if (!req.session.isAuth) {
+    return res.status(401).json({ message: "Unauthorized access!" });
+  }
+  res.json({ message: "Welcome to the session and cookies demo!" });
+});
 
 app.post("/register", async (req, res) => {
-    const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-    if(!name || !email|| !password) {
-        return res.status(400).json({ message: "Name, email and password are required" });
-    }
+  if (!name || !email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Name, email and password are required" });
+  }
 
-    const hashPassword = await bcryptjs.hash(password, 10)
+  const hashPassword = await bcryptjs.hash(password, 10);
 
-    const user = await UserModel.create({name, email, password: hashPassword})
+  const user = await UserModel.create({ name, email, password: hashPassword });
 
-    res.status(201).json({ message: "User created successfully! ", user })
-
-})
-
+  res.status(201).json({ message: "User created successfully! ", user });
+});
 
 app.post("/login", async (req, res) => {
-    const {email , password} = req.body;
+  const { email, password } = req.body;
 
-    if(!email || !password) {
-        res.status(400).json({ message: "Email or password required!"})
-    }
+  if (!email || !password) {
+    res.status(400).json({ message: "Email or password required!" });
+  }
 
-    const user = await UserModel.findOne({ email });
+  const user = await UserModel.findOne({ email });
 
-    if(!user) {
-        return res.status(400).json({ message: "Invalid email or password!"})
-    }
+  if (!user) {
+    return res.status(400).json({ message: "Invalid email or password!" });
+  }
 
-    const isMatch = await bcryptjs.compare(password, user.password)
+  const isMatch = await bcryptjs.compare(password, user.password);
 
-    if(!isMatch) {
-        res.status(400).json({ message: "Invalid Credentials !"})
-    }
+  if (!isMatch) {
+    res.status(400).json({ message: "Invalid Credentials !" });
+  }
 
-    req.session.isAuth = true;
-    res.status(200).json({ message: "Login Successfull!", user})
-})
-
+  req.session.isAuth = true;
+  res.status(200).json({ message: "Login Successfull!", user });
+});
 
 app.post("/logout", (req, res) => {
-    req.session.destroy((err)=> {
-        if(err) console.log(err)  
-    })
-    res.clearCookie("connect.sid")
-    res.json({message: "Logout Successfull!"})
-})
-
+  if (!req.session.isAuth) {
+    return res.status(400).json({ message: "You are not logged in!" });
+  }
+  req.session.destroy((err) => {
+    if (err) console.log(err);
+  });
+  res.clearCookie("connect.sid");
+  res.json({ message: "Logout Successfull!" });
+});
 
 module.exports = app;
